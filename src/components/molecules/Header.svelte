96fb2ef717e14e69483299568ddff503f79fe450
D3NCyan <edwarddoltons@gmail.com>
2021-12-17 23:20:36 +0800

    add dynamic nav in header

--- src/components/molecules/Header.svelte
+++ src/components/molecules/Header.svelte
@@ -1,11 +1,35 @@
 <script>
+    import { createEventDispatcher } from "svelte" 
+    export let navItems
+    export let activeIn
 
+    const dispatch = createEventDispatcher()
 </script>
 
 <header>
-    ini header
+    <nav>
+        <ul class="list-nav">
+            {#each navItems as item}
+                <li on:click={() => dispatch('navChange', item)}>
+                    <p class:active={item === activeIn}>{item}</p>
+                </li>
+            {/each}
+        </ul>
+    </nav>
 </header>
 
 <style>
+    ul {
+        display: flex;
+        list-style: none;
+    }
 
+    li {
+        margin-right: 2rem;
+        cursor: pointer;
+    }
+
+    .active {
+        color: red
+    }
 </style>
