f330372ebf73a12e5c10a9330eb9cdd488edbd2d
D3NCyan <edwarddoltons@gmail.com>
2021-12-18 22:08:27 +0800

    done Contact component

--- src/components/organism/index.js
+++ src/components/organism/index.js
@@ -1,5 +1,6 @@
 import Home from './Home.svelte'
 import AboutMe from './AboutMe.svelte'
 import Portfolio from './Portfolio.svelte'
+import Contact from './Contact.svelte'
 
-export { Home, AboutMe, Portfolio }
+export { Home, AboutMe, Portfolio, Contact }
