70ab1c55d833e6d5a2cb770adbd50cfc70faa806
D3NCyan <edwarddoltons@gmail.com>
2021-12-18 10:39:54 +0800

    separate HomeHero.svelte

--- src/components/organism/Home.svelte
+++ src/components/organism/Home.svelte
@@ -1,37 +1,19 @@
 <script>
+    import HomeHero from "../molecules/Home/HomeHero.svelte";
+    
     $: document.title = 'Home page'
 </script>
 
 <div class="container">
-    <div class="section section-one">
-        <h1 class="title">
-            <p>Hy!, I am</p>
-            <p>Doltons Edward N Pabiaran</p>
-        </h1>
-        <br>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
-            Asperiores, porro deleniti natus alias soluta itaque dolorem 
-        </p>
-        <br>
-        <button class="btn button-theme">Hire me</button>
-        <br>
-    </div>
-    <div class="section section-two">
-    </div>
+    <HomeHero />
+    
 </div>
 
 <style>
     .container {
         display: flex;
+        flex-wrap: wrap;
+        align-items: center;
         justify-content: space-between;
-        border: 1px solid rgba(0, 0, 0, .2);
-    }
-
-    .title {
-        font-size: 40px;
-    }
-
-    .title p {
-        line-height: 3rem;
     }
 </style>
