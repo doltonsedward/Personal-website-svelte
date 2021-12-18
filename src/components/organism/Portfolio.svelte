924702b4d0e6e078ff1610c42a030edbf82c472c
D3NCyan <edwarddoltons@gmail.com>
2021-12-18 16:10:56 +0800

    create dataPortfolio and looping it in Portfolio component

--- src/components/organism/Portfolio.svelte
+++ src/components/organism/Portfolio.svelte
@@ -1,11 +1,59 @@
 <script>
     $: document.title = 'Portfolio'
+
+    const dataPortfolio = [
+        {
+            name: "Dewetour",
+            image: "/assets/img/dewetour.png"
+        },
+        {
+            name: "Literature",
+            image: "/assets/img/literature.png"
+        },
+        {
+            name: "Rentake",
+            image: "/assets/img/rentake.png"
+        },
+    ]
 </script>
 
-<div>
-    <h1>Halo!, saya doltons Edward Nicholas</h1>
+<div class="row">
+    <div class="col-12">
+        <h1 class="text-center">Portfolio</h1>
+    </div>
+    {#each dataPortfolio as item}
+        <div class="card col-4">
+            <div class="inner-card">
+                <div class="card-media">
+                    <img src={item.image} alt={item.name}>
+                </div>
+                <div class="card-title">
+                    <p>{item.name}</p>
+                </div>
+                <div class="card-content">
+
+                </div>
+            </div>
+        </div>
+    {/each}
 </div>
 
 <style>
+    .card {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        border-radius: var(--border-trendy);
+    }
 
+    .inner-card {
+        width: 80%;
+        box-shadow: 0 5px 10px rgba(0, 0, 0, .2);
+    }
+
+    img {
+        width: 100%;
+        object-fit: cover;
+    }
 </style>
