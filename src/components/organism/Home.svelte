d065579a3fbcbb726ded403276ecfecf6f5c12c1
D3NCyan <edwarddoltons@gmail.com>
2021-12-18 09:53:53 +0800

    add hero in homepage

--- src/components/organism/Home.svelte
+++ src/components/organism/Home.svelte
@@ -1,13 +1,37 @@
 <script>
-    
-
     $: document.title = 'Home page'
 </script>
 
-<div>
-    <h1>Welcome to home</h1>
+<div class="container">
+    <div class="section section-one">
+        <h1 class="title">
+            <p>Hy!, I am</p>
+            <p>Doltons Edward N Pabiaran</p>
+        </h1>
+        <br>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+            Asperiores, porro deleniti natus alias soluta itaque dolorem 
+        </p>
+        <br>
+        <button class="btn button-theme">Hire me</button>
+        <br>
+    </div>
+    <div class="section section-two">
+    </div>
 </div>
 
 <style>
+    .container {
+        display: flex;
+        justify-content: space-between;
+        border: 1px solid rgba(0, 0, 0, .2);
+    }
 
+    .title {
+        font-size: 40px;
+    }
+
+    .title p {
+        line-height: 3rem;
+    }
 </style>
