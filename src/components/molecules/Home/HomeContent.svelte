ae792c2299c3a80d47190cc01772267fa26faf3c
D3NCyan <edwarddoltons@gmail.com>
2021-12-18 20:07:42 +0800

    use AOS in HomeContent component

--- src/components/molecules/Home/HomeContent.svelte
+++ src/components/molecules/Home/HomeContent.svelte
@@ -1,8 +1,8 @@
 <script>
-    import AOS from 'aos';
-    import 'aos/dist/aos.css'; 
+    import AOS from 'aos'
+    import 'aos/dist/aos.css' 
     
-    AOS.init();
+    AOS.init()
 </script>
 
 <div class="content">
