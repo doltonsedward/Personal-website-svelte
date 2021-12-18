e3d590db7d02c7cf265700185ca1ee93ac5911cc
D3NCyan <edwarddoltons@gmail.com>
2021-12-18 22:08:12 +0800

    done Contact component

---
+++ src/components/organism/Contact.svelte
@@ -0,0 +1,24 @@
+<script>
+    $: document.title = 'Contact'
+</script>
+
+<div class="row">
+    <div class="col-12">
+        <iframe
+            id="JotFormIFrame-213514020184038"
+            title="Contact me"
+            onload="window.parent.scrollTo(0,0)"
+            allowtransparency="true"
+            allowfullscreen="true"
+            allow="geolocation; microphone; camera"
+            src="https://form.jotform.com/213514020184038"
+            frameborder="0"
+            style="
+            min-width: 100%;
+            height:539px;
+            border:none;"
+            scrolling="no"
+        >
+        </iframe>
+    </div>
+</div>
