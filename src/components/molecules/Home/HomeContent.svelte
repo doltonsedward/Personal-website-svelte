5fc699b9e8c198183ac0f2490664d3a288478bde
D3NCyan <edwarddoltons@gmail.com>
2021-12-18 10:59:44 +0800

    prepare for HomeContent.svelte

---
+++ src/components/molecules/Home/HomeContent.svelte
@@ -0,0 +1,10 @@
+<div class="content">
+    <div class="row">
+        <p class="col-3">Satu</p>
+        <p class="col-3">Satu</p>
+        <p class="col-12">Satu</p>
+    </div>
+</div>
+
+<style>
+</style>
