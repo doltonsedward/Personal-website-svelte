cbd8c070315909667c762ffff0301d3bc7870043
D3NCyan <edwarddoltons@gmail.com>
2021-12-20 18:23:05 +0800

    remove dataProfile in src/script.js

--- src/script.js
+++ src/script.js
@@ -42,38 +42,4 @@ const redirect = (target, targetName) => {
     }, 1500);
 }
 
-// data portfolio
-const dataPortfolio = [
-    {
-        name: "Dewetour",
-        description: "Dewetour is an application to make it easier to book travel tickets online and quickly without the need to come to a travel provider",
-        image: "/assets/img/dewetour.png",
-        url: "https://github.com/doltonsedward/Dewe_Tour"
-    },
-    {
-        name: "Literature",
-        description: "Literature is a web-based application that allows you to write your work and be seen by many people",
-        image: "/assets/img/literature.png",
-        url: "https://github.com/doltonsedward/Literature"
-    },
-    {
-        name: "Rentake",
-        description: "Rentake is a website-based application that allows you to buy movie tickets easily",
-        image: "/assets/img/rentake.png",
-        url: "https://github.com/doltonsedward/Rentake"
-    },
-    {
-        name: "IT Lance",
-        description: "IT Lance is a place for job seekers with a light and unique look. come up with an exciting new style.",
-        image: "/assets/img/it-lance.png",
-        url: "https://github.com/doltonsedward/IT_Lance-App"
-    },
-    {
-        name: "DollosChatbot",
-        description: "Platform that lets you write messages with bots whatever you want to do without humans knowing",
-        image: "/assets/img/dollos-chatbot.png",
-        url: "https://github.com/doltonsedward/DollosChatbot"
-    },
-]
-
-export { buttonRipple, redirect, dataPortfolio }
+export { buttonRipple, redirect }
