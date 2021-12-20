042e072b341efbe3fb63e776d3f6ead8bc77e2c6
D3NCyan <edwarddoltons@gmail.com>
2021-12-20 11:07:09 +0800

    create data in js folder

---
+++ public/assets/js/data.js
@@ -0,0 +1,21 @@
+// data skill
+const dataSkill = [
+	{ name: "JavaScript", level: "Advanced" },
+	{ name: "PHP", level: "Intermediate" },
+	{ name: "React", level: "Advanced" },
+	{ name: "React Native", level: "Novice" },
+	{ name: "Laravel", level: "Novice" },
+	{ name: "Svelte", level: "Intermediate" },
+	{ name: "Socket.io", level: "Intermediate" },
+	{ name: "Express", level: "Advanced" },
+	{ name: "ORM & ODB", level: "Intermediate" },
+]
+
+// data tools
+const dataTools = [
+	{ name: "Adobe XD", level: "Intermediate" },
+	{ name: "Inkscape", level: "Expert" },
+	{ name: "Git", level: "Advanced" },
+]
+
+export { dataSkill, dataTools }
