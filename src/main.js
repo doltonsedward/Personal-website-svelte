80ef89a78b76baafd05ee76c544b9a246563775c
D3NCyan <edwarddoltons@gmail.com>
2021-12-20 18:18:47 +0800

    add data in props, main.js

--- src/main.js
+++ src/main.js
@@ -1,9 +1,13 @@
 import App from './App.svelte';
+import { dataSkill, dataTools, dataPortfolio } from '../public/assets/js/data'
 
 const app = new App({
 	target: document.body,
 	props: {
-		title: 'Home page'
+		title: 'Home page',
+		dataSkill,
+		dataTools,
+		dataPortfolio
 	}
 });
 
