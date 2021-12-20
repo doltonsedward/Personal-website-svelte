170148df7a9c49c22f50cfab0f66354322d35602
D3NCyan <edwarddoltons@gmail.com>
2021-12-20 18:23:37 +0800

    remove local dataSkill, use global dataSkill instead

--- src/components/molecules/Home/HomeContent.svelte
+++ src/components/molecules/Home/HomeContent.svelte
@@ -4,23 +4,7 @@
     
     AOS.init()
 
-    const dataSkill = [
-        { name: "JavaScript", level: "Advanced" },
-        { name: "PHP", level: "Intermediate" },
-        { name: "React", level: "Advanced" },
-        { name: "React Native", level: "Novice" },
-        { name: "Laravel", level: "Novice" },
-        { name: "Svelte", level: "Intermediate" },
-        { name: "Socket.io", level: "Intermediate" },
-        { name: "Express", level: "Advanced" },
-        { name: "ORM & ODB", level: "Intermediate" },
-    ]
-
-    const dataTools = [
-        { name: "Adobe XD", level: "Intermediate" },
-        { name: "Inkscape", level: "Expert" },
-        { name: "Git", level: "Advanced" },
-    ]
+    export let dataSkill, dataTools
 </script>
 
 <div class="content">
