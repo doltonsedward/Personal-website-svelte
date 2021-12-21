fdc90b4a667b7a65364d7a1eb2c9340514a815be
D3NCyan <edwarddoltons@gmail.com>
2021-12-21 10:47:41 +0800

    add real loading effect when fetching JotForm

--- src/components/organism/Contact.svelte
+++ src/components/organism/Contact.svelte
@@ -1,13 +1,28 @@
 <script>
     $: document.title = 'Contact'
+    let loading = true
 </script>
 
 <div class="row">
     <div class="col-12">
+        {#if loading}
+            <div class="loading">
+                <div class="middle">
+                    <div class="bar bar1"></div>
+                    <div class="bar bar2"></div>
+                    <div class="bar bar3"></div>
+                    <div class="bar bar4"></div>
+                    <div class="bar bar5"></div>
+                    <div class="bar bar6"></div>
+                    <div class="bar bar7"></div>
+                    <div class="bar bar8"></div>
+                </div>
+            </div>
+        {/if}
         <iframe
             id="JotFormIFrame-213514020184038"
             title="Contact me"
-            onload="window.parent.scrollTo(0,0)"
+            on:load={()=> loading = false}
             allowtransparency="true"
             allowfullscreen="true"
             allow="geolocation; microphone; camera"
@@ -22,3 +37,55 @@
         </iframe>
     </div>
 </div>
+
+<style>
+    .loading {
+        width: 100%;
+        height: 100vh;
+        background-color: rgba(29, 29, 39, .9);
+        position: fixed;
+        left: 0;
+        top: 0;
+    }
+    .middle {
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        position: absolute;
+    }
+
+    .bar {
+        width: 10px;
+        height: 70px;
+        display: inline-block;
+        transform-origin: bottom center;
+        border-top-right-radius: 20px;
+        border-top-left-radius: 20px;
+        animation: loader 1.2s linear infinite;
+    }
+    
+    .bar1 { animation-delay: 0.1s }
+    .bar2 { animation-delay: 0.2s }
+    .bar3 { animation-delay: 0.3s }
+    .bar4 { animation-delay: 0.4s }
+    .bar5 { animation-delay: 0.5s }
+    .bar6 { animation-delay: 0.6s }
+    .bar7 { animation-delay: 0.7s }
+    .bar8 { animation-delay: 0.8s }
+
+    @keyframes loader {
+        0% {
+            transform: scaleY(0.1);
+            background: none;
+        }
+        50% {
+            transform: scaleY(1);
+            background: var(--color-theme);
+        }
+        100% {
+            transform: scaleY(0.1);
+            background: transparent;
+        }
+    }
+
+</style>
